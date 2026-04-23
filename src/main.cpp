5bde5ca33e53c8f733d6ca2f2b4ff413c4b02410
dannifaze-code <dannifaze@gmail.com>
2026-04-23 15:13:45 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,11 @@
-#include <QApplication>\n#include "MainWindow.h"\n\nint main(int argc, char *argv[]) {\n    QApplication app(argc, argv);\n    MainWindow window;\n    window.show();\n    return app.exec();\n}
+SDL_Event event;
+while (SDL_PollEvent(&event)) {
+    // If the event was a keypress, the ControllerManager handles it
+    controllerManager.processEvent(event);
+}
+
+// Send the current button states to the emulator before running the frame
+emulatorCore.setInputMask(controllerManager.getKeyMask());
+
+// Run the frame!
+emulatorCore.runFrame();
