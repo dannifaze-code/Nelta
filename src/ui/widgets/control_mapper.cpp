5fb8e02ff32f59a4285c8b6994fe81aa36f6d819
dannifaze-code <dannifaze@gmail.com>
2026-04-23 14:43:12 -0400

    Create ControlMapper widget implementation in control_mapper.cpp

---
+++ src/ui/widgets/control_mapper.cpp
@@ -0,0 +1,14 @@
+#include <QWidget>
+
+class ControlMapper : public QWidget {
+    Q_OBJECT
+
+public:
+    explicit ControlMapper(QWidget *parent = nullptr) : QWidget(parent) {
+        // Constructor implementation
+    }
+
+    // Define other necessary methods and functionalities
+};
+
+// Add other widget functionalities as needed
