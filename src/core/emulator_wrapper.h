46466ec6d0c8bf3432f2a289c7d0d056a4d139f4
dannifaze-code <dannifaze@gmail.com>
2026-04-23 15:11:14 -0400

    Update emulator_wrapper.h

--- src/core/emulator_wrapper.h
+++ src/core/emulator_wrapper.h
@@ -33,3 +33,38 @@ private:
 };
 
 #endif // EMULATOR_WRAPPER_H
+#ifndef EMULATOR_WRAPPER_H
+#define EMULATOR_WRAPPER_H
+
+#include <string>
+#include <vector>
+#include <cstdint>
+#include "../audio/audio_engine.h"
+
+namespace melonDS {
+    class NDS; 
+}
+
+class EmulatorCore {
+public:  
+    EmulatorCore();
+    ~EmulatorCore();
+
+    bool loadROM(const std::string &romPath);
+    void runFrame();
+    const uint32_t* getFramebuffer() const;
+    
+    // Pass the input state from the ControllerManager to the Core
+    void setInputMask(uint32_t keyMask);
+    
+    void reset();
+    bool isRunning() const;
+
+private:  
+    melonDS::NDS* melonDSInstance;
+    bool running;
+
+    AudioEngine audioEngine;
+};
+
+#endif // EMULATOR_WRAPPER_H
